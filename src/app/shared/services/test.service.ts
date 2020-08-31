26542b25e2cbc174353440889e65199cd88a0b5a
npnguyenan <np.nguyenan@gmail.com>
2020-08-31 13:23:04 +0700

    sales-manage-app

--- src/app/shared/services/test.service.ts
+++ src/app/shared/services/test.service.ts
@@ -11,12 +11,11 @@ export class TestService {
 
    }
    getApi(){
-      // const url='http://api.openweathermap.org/data/2.5/find?units=metric&appid=3d029b9440d5d46824bb211b386f5f4d&q=london';
+      // const url='http://api.openweathermap.org/data/2.5/find?units=metric&appid=3d029b9440d5d46824bb211b386f5f4d&q=saigon';
       const url='http://localhost:3000/companies';
       return this.http.get(url)
        .toPromise()
-       .then(res => res.json())
+       .then(res => res.json());
       //  .then(resJson => resJson.list);
-       .then(resJson => resJson.companies);
    }
 }
